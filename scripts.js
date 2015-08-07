88a80018aab0f0bc945d3ce5a50ebe6459dd4bd8
zoejf <zoejfoss@gmail.com>
2015-08-06 20:24:42 -0700

    fixed email address

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
 $(function() {
    
-  //when you hover over nav icons
-  $("fa").hover(function() {
-    //change color from teal to grey
-    $(this).addClass("changeColor");
-  }, function() {
-    $(this).removeClass("changeColor");
-  }); 
+  // //when you hover over nav icons
+  // $("fa").hover(function() {
+  //   //change color from teal to grey
+  //   $(this).addClass("changeColor");
+  // }, function() {
+  //   $(this).removeClass("changeColor");
+  // }); 
 
 
 });
